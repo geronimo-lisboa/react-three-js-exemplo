2d995fbbfbc675d651fc916144693c4aecf55d04
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-07-30 16:12:50 -0300

    Tentativa de por as coisas no state deu errado. Não renderiza nem no loop de renderização do animate

--- src/App.js
+++ src/App.js
@@ -2,132 +2,99 @@ import React, { Component } from 'react';
 //import threeEntryPoint from  './threeEntryPoint.js' //'./src/threeEntryPoint';
 import * as THREE from 'three'
 // //https://medium.com/@necsoft/three-js-101-hello-world-part-1-443207b1ebe1
-// class MyThreeJSContext extends Component{
-//     constructor(props){
-//         super(props);
-//         this.state = {threejsInitialized: false};
-//         this.canvasRef = React.createRef();//serve pra criar a referência pra div, necessária para a construção das coisas
-//         //do threejs
-//         this.buildThreeJSObjects = this.buildThreeJSObjects.bind(this);
-//         this.animation = this.animation.bind(this);
-//     }
-//
-//     buildThreeJSObjects(){
-//         console.log("Aqui vou construir o contexto do threejs");
-//         let width = this.canvasRef.current.clientWidth;
-//         let height = this.canvasRef.current.clientHeight;
-//         let canvas = this.canvasRef.current;
-//         //Criação do renderer
-//         let renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
-//         //Pra lidar com as diferenças entre telas normais e telas tipo as Retina
-//         const DPR = window.devicePixelRatio ? window.devicePixelRatio : 1;
-//         renderer.setPixelRatio(DPR);
-//         renderer.setSize(width, height);
-//         //Com isso renderer está criado e setado.
-//         //Cria a câmera
-//         //propriedades da câmera
-//         const aspectRatio = width / height;
-//         const fieldOfView = 60;
-//         const nearPlane = 4;
-//         const farPlane = 100;
-//         //cria a câmera
-//         let camera = new THREE.PerspectiveCamera(fieldOfView, aspectRatio, nearPlane, farPlane);
-//         //posiciona a câmera
-//         camera.position.z = 40;
-//         //Com isso a câmera está criada
-//         //Cria a cena
-//         let scene = new THREE.Scene();
-//         scene.background = new THREE.Color("#00ff00");
-//         //Cria o objeto 3d
-//         let geometry = new THREE.BoxGeometry(10,10,10);
-//         let material = new THREE.MeshBasicMaterial({color: '#ff0000'});
-//         let cube = new THREE.Mesh(this.geometry, this.material);
-//         scene.add(cube);
-//         this.setState({
-//             threejsInitialized:true,
-//             renderer : renderer,
-//             camera : camera,
-//             scene : scene,
-//             geometry : geometry,
-//             material: material,
-//             cube: cube
-//         });
-//
-//     }
-//
-//     componentDidMount(){
-//         if(this.state.threejsInitialized===false){
-//             this.buildThreeJSObjects();
-//         }
-//     }
-//
-//     animation(){
-//         if(this.state.threejsInitialized===true)
-//         {
-//
-//             window.requestAnimationFrame( this.animation );
-//             this.state.renderer.render( this.state.scene, this.state.camera);
-//         }
-//     }
-//
-//     render(){
-//         window.requestAnimationFrame( this.animation );
-//         return(<div>
-//             <canvas ref={this.canvasRef}></canvas>
-//         </div>);
-//     }
-// }
-
 class MyThreeJS extends Component{
     constructor(props){
         super(props);
         this.canvasRef = React.createRef();
         this.init = this.init.bind(this);
         this.animation = this.animation.bind(this);
-        this.camera = undefined;
-        this.scene = undefined;
-        this.renderer = undefined;
-        this.geometry = undefined;
-        this.material = undefined;
-        this.mesh = undefined;
-        this.isInitiated = false;
+        this.createCamera = this.createCamera.bind(this);
+        this.createScene = this.createScene.bind(this);
+        this.state = {isInitiated:false, renderContext:undefined, obj:undefined};
+        // this.camera = undefined;
+        // this.scene = undefined;
+        // this.renderer = undefined;
+        // this.geometry = undefined;
+        // this.material = undefined;
+        // this.mesh = undefined;
+        // this.isInitiated = false;
+    }
+
+    createCamera(){
+        const cam = new THREE.PerspectiveCamera(
+            75, this.canvasRef.current.clientWidth / this.canvasRef.current.clientHeight, 1, 1000 );
+        return cam;
+    }
+
+    createScene(){
+        const scene = new THREE.Scene();
+        return scene;
+    }
+
+    create3dObject(){
+        let geometry = new THREE.IcosahedronGeometry(200, 1 );
+        let material = new THREE.MeshBasicMaterial({
+            color: 0xfff999fff,
+            wireframe: true,
+            wireframelinewidth:8 });
+        let mesh = new THREE.Mesh(this.geometry, this.material);
+        const obj = {geometry:geometry, material:material, mesh:mesh};
+        return obj;
+    }
+
+    createRenderer(canvas){
+        let newRenderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
+        newRenderer.setSize( canvas.clientWidth, canvas.clientHeight ) ;
+        return newRenderer;
     }
 
     init(){
-        this.camera = new THREE.PerspectiveCamera(
-            75, this.canvasRef.current.clientWidth / this.canvasRef.current.clientHeight, 1, 1000 );
-        this.camera.position.z = 500;
-        this.scene = new THREE.Scene();
-        this.geometry = new THREE.IcosahedronGeometry(200, 1 );
-        this.material =  new THREE.MeshBasicMaterial({
-            color: 0xfff999fff,
-            wireframe: true,
-            wireframelinewidth:8 })
-        this.mesh = new THREE.Mesh(this.geometry, this.material);
-        this.scene.add( this.mesh );
-        this.renderer = new THREE.WebGLRenderer({ canvas: this.canvasRef.current, antialias: true, alpha: true })
-        this.renderer.setSize( this.canvasRef.current.clientWidth, this.canvasRef.current.clientHeight ) ;
-        this.isInitiated = true;
+        let newCam = this.createCamera();
+        newCam.position.z = 500;
+        let newScene = this.createScene();
+        let newObj3D = this.create3dObject();
+        newScene.add(newObj3D.mesh);
+        newScene.backgroundColor = '#ff0000';
+        let newRenderer = this.createRenderer(this.canvasRef.current);
+        //{isInitiated:false, renderContext:undefined, obj:undefined};
+        let newState = {isInitiated:true, renderContext:{
+                camera:newCam,
+                scene:newScene,
+                renderer:newRenderer
+            },
+            obj: newObj3D
+        };
+        this.setState(newState);
     }
 
     animation(){
         window.requestAnimationFrame( this.animation );
 
-        this.mesh.rotation.x = Date.now() * 0.00005;
-        this.mesh.rotation.y = Date.now() * 0.0001;
-        this.renderer.render( this.scene, this.camera);
+        //this.mesh.rotation.x = Date.now() * 0.00005;
+        //this.mesh.rotation.y = Date.now() * 0.0001;
+        this.state.renderContext.renderer.render(
+            this.state.renderContext.scene,
+            this.state.renderContext.camera
+        );
+        //this.renderer.render( this.scene, this.camera);
     }
     //DO REACT
     componentDidMount(){
-        if(this.isInitiated===false){
+        if(this.state.isInitiated===false){
             this.init();
-            this.animation();
+            //this.animation();
         }
     }
     //Render - DO REACT
     render(){
-        // if(this.isInitiated===true)
+        // if(this.state.isInitiated===true)
         //     this.animation();
+        if(this.state.renderContext!=undefined){
+            this.state.renderContext.renderer.render(
+                this.state.renderContext.scene,
+                this.state.renderContext.camera
+            );
+        }
         return(
             <div>
                 <canvas ref={this.canvasRef}></canvas>
@@ -138,25 +105,12 @@ class MyThreeJS extends Component{
 
 class App extends Component {
     render(){
-        ////da coisa q eu baixei e q funciona
-        // return(
-        //     <div>
-        //         <div ref={element=>this.threeRootElement = element}/>
-        //         <hr/>
-        //         <MyThreeJSContext/>
-        //     </div>
-        // )
         return(
             <div>
             <MyThreeJS/>
             </div>
         )
     }
-    /// da coisa que eu baixei e que funciona
-    // componentDidMount(){
-    //     threeEntryPoint(this.threeRootElement);
-    // }
-
 }
 
 export default App;
