4d113dd853f0b3f9ed09457cb0bc7e596e70ce68
geronimo-lisboa <erikaeluciano2016@gmail.com>
2018-07-30 12:51:19 -0300

    Background verde mas n renderiza o cubo.

--- src/App.js
+++ src/App.js
@@ -1,25 +1,21 @@
 import React, { Component } from 'react';
-import threeEntryPoint from  './threeEntryPoint.js' //'./src/threeEntryPoint';
+//import threeEntryPoint from  './threeEntryPoint.js' //'./src/threeEntryPoint';
 import * as THREE from 'three'
 //https://medium.com/@necsoft/three-js-101-hello-world-part-1-443207b1ebe1
 class MyThreeJSContext extends Component{
     constructor(props){
         super(props);
         this.state = {threejsInitialized: false};
-        this.divRef = React.createRef();//serve pra criar a referência pra div, necessária para a construção das coisas
+        this.canvasRef = React.createRef();//serve pra criar a referência pra div, necessária para a construção das coisas
         //do threejs
         this.buildThreeJSObjects = this.buildThreeJSObjects.bind(this);
     }
 
     buildThreeJSObjects(){
         console.log("Aqui vou construir o contexto do threejs");
-        let width = this.divRef.current.clientWidth;
-        let height = this.divRef.current.clientHeight;
-        //Criação da canvas na div. TODO: Criar a canvas no Render() e pegar a ref pra canvas.
-        let canvas = document.createElement('canvas');
-        canvas.width = width;
-        canvas.height = height;
-        this.divRef.current.appendChild(canvas);
+        let width = this.canvasRef.current.clientWidth;
+        let height = this.canvasRef.current.clientHeight;
+        let canvas = this.canvasRef.current;
         //Criação do renderer
         let renderer = new THREE.WebGLRenderer({ canvas: canvas, antialias: true, alpha: true });
         //Pra lidar com as diferenças entre telas normais e telas tipo as Retina
@@ -41,19 +37,19 @@ class MyThreeJSContext extends Component{
         //Cria a cena
         let scene = new THREE.Scene();
         scene.background = new THREE.Color("#00ff00");
-
-        // //teste da canvas
-        // var ctx = canvas.getContext("2d");
-        // ctx.beginPath();
-        // ctx.rect(0, 0, 150, 100);
-        // ctx.fillStyle = "red";
-        // ctx.fill();
-
+        //Cria o objeto 3d
+        let geometry = new THREE.BoxGeometry(10,10,10);
+        let material = new THREE.MeshBasicMaterial({color: '#ff0000'});
+        let cube = new THREE.Mesh(this.geometry, this.material);
+        scene.add(cube);
         this.setState({
             threejsInitialized:true,
             renderer : renderer,
             camera : camera,
-            scene : scene
+            scene : scene,
+            geometry : geometry,
+            material: material,
+            cube: cube
         });
 
     }
@@ -65,28 +61,33 @@ class MyThreeJSContext extends Component{
     }
 
     render(){
-        //requestAnimationFrame(this.render);
         if(this.state.threejsInitialized===true)
-            this.state.renderer.render(this.state.scene, this.state.camera);
-        return(<div ref={this.divRef}>foobar</div>)
+             this.state.renderer.render(this.state.scene, this.state.camera);
+        return(<div>
+            <canvas ref={this.canvasRef}></canvas>
+        </div>);
     }
 }
 
 class App extends Component {
     render(){
+        // return(
+        //     <div>
+        //         <div ref={element=>this.threeRootElement = element}/>
+        //         <hr/>
+        //         <MyThreeJSContext/>
+        //     </div>
+        // )
         return(
             <div>
-                <div ref={element=>this.threeRootElement = element}/>
-                <hr/>
-                <MyThreeJSContext/>
+            <MyThreeJSContext/>
             </div>
-
         )
     }
 
-    componentDidMount(){
-        threeEntryPoint(this.threeRootElement);
-    }
+    // componentDidMount(){
+    //     threeEntryPoint(this.threeRootElement);
+    // }
 
 }
 
